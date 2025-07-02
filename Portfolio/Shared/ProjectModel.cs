795d253b9d3120b39cf3ea70880ed0c5b6b46c60
Sacha Machado Albino <134261807+ZetoxX7@users.noreply.github.com>
2025-07-02 13:15:34 +0200

    v1

---
+++ Portfolio/Shared/ProjectModel.cs
@@ -0,0 +1,10 @@
+namespace Shared
+{
+    public class ProjectModel
+    {
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string ImageUrl { get; set; } = "";
+        public string GitHubUrl { get; set; } = "";
+    }
+}
